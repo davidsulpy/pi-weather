1d78d468bd95638d6c3eb23b3118695cbc0e8bb3
David Sulpy <david@sulpy.com>
2015-03-09 18:33:33 -0500

    initial sensor-less test commit

---
+++ app.py
@@ -0,0 +1,17 @@
+from ISStreamer.Streamer import Streamer
+import time
+import os
+
+bucketKey = os.getenv('IS_BUCKET_KEY', 'resinio_temp_mon_test')
+bucketName = os.getenv('IS_BUCKET_NAME', 'resin.io temp mon')
+accessKey = os.getenv('IS_ACCESS_KEY', '')
+
+streamer = Streamer(bucket_name=bucketName, bucket_key=bucketKey, access_key=accessKey)
+
+for x in range(0, 300):
+	streamer.log('iteration', x)
+	streamer.log('temperature(F)', 72)
+
+	time.sleep(.1)
+
+streamer.close()
